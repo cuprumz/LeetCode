33670d7136d111c992549750cc550b87ef1522f9
cuprumz@Annie <cuprumzhen@gmail.com>
2020-01-19 23:12:04 +0800

    feat(easy): merge two sorted lists

---
+++ src/merge_two_sorted_lists/README.md
@@ -0,0 +1,21 @@
+### merge two sorted lists
+
+```
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+```
+
+
+```c++
+l1 = l1->next; // 在链表l1上移动
+```
+
+```c++
+l2 = l2->next; // 同理，在l2上移动
+```
+
+TODO
---
+++ src/merge_two_sorted_lists/merge_two_sorted_lists.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode *next;
+
+  ListNode(int x): val(x), next(NULL) {}
+
+  friend ostream &operator<<(ostream &out, ListNode &node);
+};
+
+ostream &operator<<(ostream &out, ListNode &node) {
+
+    out << "val: " << node.val << ", addr: " << &node;
+
+    return out;
+}
+
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2);
+};
+
+
+ListNode * Solution::mergeTwoLists(ListNode *l1, ListNode *l2) {
+    ListNode *head = NULL;
+    ListNode **pTail = &head;
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->val < l2->val) {
+            *pTail = l1;
+            l1 = l1->next;
+        } else {
+            *pTail = l2;
+            l2 = l2->next;
+        }
+        pTail = &(*pTail)->next;
+    }
+    *pTail = (l1 != NULL ? l1 : l2);
+    return head;
+}
+
+void printListNode(ListNode *head) {
+    if (head == NULL) return;
+    ListNode *p = head;
+    while (p != NULL) {
+        cout << *p;
+        p = p->next;
+        if (p != NULL) {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+}
+
+int main(int argc, char **argv) {
+
+    ListNode l0(1), l1(2), l2(4), l3(1), l4(3), l5(4);
+    l0.next = &l1;
+    l1.next = &l2;
+    l3.next = &l4;
+    l4.next = &l5;
+
+    printListNode(&l0);
+    printListNode(&l3);
+
+    Solution s;
+    ListNode *p = s.mergeTwoLists(&l0, &l3);
+    printListNode(p);
+
+    return 0;
+}

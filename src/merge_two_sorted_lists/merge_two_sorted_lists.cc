29e544d5fe46c1e9a00c2237dac1463db87ee874
cuprumz@Reiner <cuprumzhen@gmail.com>
2020-04-01 10:53:32 +0800

    docs(merge two sorted lists): add comments

--- src/merge_two_sorted_lists/merge_two_sorted_lists.cc
+++ src/merge_two_sorted_lists/merge_two_sorted_lists.cc
@@ -23,21 +23,24 @@ public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2);
 };
 
-
+/**
+ * head指针 指向头节点
+ * 改变二级指针的指向，使得*pTail每次指向下一个位置
+ */
 ListNode *Solution::mergeTwoLists(ListNode *l1, ListNode *l2) {
     ListNode *head = NULL;
-    ListNode **pTail = &head;
+    ListNode **pTail = &head; // 准备好头节点的位置
     while (l1 != NULL && l2 != NULL) {
         if (l1->val < l2->val) {
             *pTail = l1;
             l1 = l1->next;
         } else {
-            *pTail = l2;
-            l2 = l2->next;
+            *pTail = l2; // l2赋值给head, 使得head指向l2所指向的节点
+            l2 = l2->next; // l2->next赋值给l2, 使得l2指向l2->next所指向的节点
         }
-        pTail = &(*pTail)->next;
+        pTail = &(*pTail)->next; // pTail指向head->next 下一次改变*pTail相当于 在已经排序的链后面挂上
     }
-    *pTail = (l1 != NULL ? l1 : l2);
+    *pTail = (l1 != NULL ? l1 : l2); // 挂上剩下的
     return head;
 }
 
@@ -57,7 +60,7 @@ void printListNode(ListNode *head) {
 ListNode *mergeSortedListsV2(ListNode *l1, ListNode *l2) {
     if (l1 == NULL || l2 == NULL) return NULL;
     ListNode dummy(-1);
-    ListNode *p = &dummy;
+    ListNode *p = &dummy; // p->next is ready
 
     while (l1 != NULL && l2 != NULL) {
         if (l1->val < l2->val) {
@@ -67,7 +70,7 @@ ListNode *mergeSortedListsV2(ListNode *l1, ListNode *l2) {
             p->next = l2;
             l2 = l2->next;
         }
-        p = p->next;
+        p = p->next; // p move next
     }
 
     p->next = l1 != NULL ? l1 : l2;
@@ -87,6 +90,7 @@ int main(int argc, char **argv) {
 
     Solution s;
     ListNode *p = s.mergeTwoLists(&l0, &l3);
+//    ListNode *p = mergeSortedListsV2(&l0, &l3);
     printListNode(p);
 
     return 0;
